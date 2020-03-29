54e8e2161eb92eee1268dcc5619af46d90c11b27
Jatin Kathuria <jtn.kathuria@gmail.com>
2020-03-29 23:07:15 +0530

    Initial Commit

---
+++ custom_fields.php
@@ -0,0 +1,18 @@
+<?
+
+function run(){
+ $hobbies = [
+                //      'type'        => \WPGraphQL\Types::list_of( \WPGraphQL\Types::string() ),
+                        'type'        => 'String',
+                        'description' => __( 'Custom field for user mutations', 'your-textdomain' ),
+                        'resolve'     => function( $cat ) {
+                                $thumbId = get_term_meta($cat->categoryId, 'category-image-id', true );
+                                $thumb_src = wp_get_attachment_url($thumbId);
+                                return ! empty( $thumb_src ) ? $thumb_src : null ;
+                        },
+                ];
+
+                register_graphql_field( 'Category', 'thumbnail', $hobbies );
+
+
+}
